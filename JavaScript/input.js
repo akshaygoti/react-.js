4389fbe2b3be0b439de2f01ca882900172706fde
Akshay <akshaygoti23@gmail.com>
2024-01-18 09:31:35 +0530

    18-01-24

---
+++ JavaScript/input.js
@@ -0,0 +1,34 @@
+console.log("Wlcome to Javascript");
+
+var number1=20;
+var number2=20;
+var number3=30;
+console.log(number1+number2-number3);
+
+
+{
+    var newelement = document.createElement("p");
+    newelement.textContent="hello world h2";
+    document.body.appendChild(newelement);
+
+    var newelement = document.createElement("h3");
+    newelement.textContent="how are you h3";
+    document.body.appendChild(newelement);
+
+    var newelement = document.createElement('h2');
+    newelement.textContent="This is JAvascript";
+    document.body.appendChild(newelement)
+}
+
+// template literals
+
+// var _namesone =20-20 -'5';
+// var _namestow ='5';
+// console.log(_namesone)
+// console.log(type(_namesone));
+
+// var one=20
+// var tow=40
+
+// var three="one 'two'"
+// console.log(three);

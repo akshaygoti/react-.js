6be0d0296ba81e408142f715247260ea849dee63
Akshay <akshaygoti23@gmail.com>
2024-01-19 08:13:26 +0530

    19-01-24

--- JavaScript/input.js
+++ JavaScript/input.js
@@ -32,3 +32,53 @@ console.log(number1+number2-number3);
 
 // var three="one 'two'"
 // console.log(three);
+
+
+
+/*Declaration var()*/
+
+// This is Global Variables
+
+var z = 40;
+{
+    var x = 20;
+    console.log(x);
+}
+
+var y = 70;
+var z = 80;
+console.log(y,z);
+
+
+/* Declaration let() */
+
+// This is Block-scope variables
+
+{
+    // let x = "hello";
+    let x = 'java';
+    x = "Javascript"
+    console.log(x);
+}
+
+{ 
+    let x = 'sun' 
+    console.log(x);
+}
+
+{
+    let y = 'This day to Javascript'
+    console.log(y)
+}
+
+/* Declaration const() */
+
+{const z = 30;
+console.log(z);}
+
+/* String with quotes */
+
+let x = "wor'ld";
+console.lon(x);
+
+

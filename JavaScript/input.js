f418ac6aeb2a4da54cceffb8958793e63051b4c9
Akshay <akshaygoti23@gmail.com>
2024-01-24 16:54:05 +0530

    24-01-24

--- JavaScript/input.js
+++ JavaScript/input.js
@@ -166,32 +166,32 @@ console.log("Wlcome to Javascript");
 
 // Arithmetic Opreter
 
-let a = 4;
-let b = 2;
+// let a = 4;
+// let b = 2;
 
-console.log(a + b);
+// console.log(a + b);
 
-console.log(a - b);
+// console.log(a - b);
 
-console.log(a * b);
+// console.log(a * b);
 
-console.log(a / b);
+// console.log(a / b);
 
-console.log(a % b);
+// console.log(a % b);
 
-console.log(a ** b);
+// console.log(a ** b);
 
 // console.log(a++);
-console.log(++a);
+// console.log(++a);
 
 // console.log(b++);
-console.log(++b);
+// console.log(++b);
 
 // console.log(a--);
-console.log(--a);
+// console.log(--a);
 
 // console.log(b--);
-console.log(--b);
+// console.log(--b);
 
 // ********************************************
 
@@ -200,32 +200,264 @@ console.log(--b);
 
 // Check if a number is odd or even in JavaScript
 
-const number = 10;
+// const number = 10;
 
 //check if the number is even
 
-if(number % 2 == 0) {
-    console.log("The number is even.");
-}
+// if(number % 2 == 0) {
+//     console.log("The number is even.");
+// }
 
-// chack if the number is odd
+// // chack if the number is odd
 
-else {
-    console.log("The number is odd.");
-}
+// else {
+//     console.log("The number is odd.");
+// }
 
 
 // program 2
 
+// {
+//     number1 = 10;
+//     number2 = 20;
+
+// if (number1 > number2) {
+//     console.log(" bigger = number1");
+// }
+
+// else{
+//     console.log("bigger = number2");
+// }
+
+// }
+
+
+
+// program 3
+
+//  Number + Number => Addition 
+// let x = 1 + 2 
+// console.log(x) 
+
+//  Number + String => Concatenation 
+// let y = 5 + "hello"
+// console.log(y) 
+
+// let z = 10;
+// let x = 20;
+
+// document.write(z+x +"<br>");
+
+// document.write(z-x +"<br>");
+
+// document.write(z*x +"<br>");
+
+// document.write(z/x +"<br>");
+
+// document.write(z%x +"<br>");
+
+// document.write(z**x +"<br>");
+
+// document.write(z++ +"<br>");
+// document.write(++z +"<br>");
+
+// document.write(x++ +"<br>");
+// document.write(++x +"<br>");
+
+// document.write(z-- +"<br>");
+// document.write(--z +"<br>");
+
+// document.write(x-- +"<br>");
+// document.write(--x +"<br>");
+
+// program 4
+
+// let marks=79;
+
+// if(marks>=90){
+//     console.log("Grade A");
+// }
+
+// else if(marks>=80){
+//     console.log("Grade B");
+// }
+
+// else if(marks>=70){
+//     console.log("Grade C");
+// }
+
+// else if(marks>=60){
+//     console.log("Grade D");
+// }
+
+// else{
+//     console.log("You are fail");
+// }
+
+
+/* Javascript Loops */
+
+// Javascript while Loop //
+
+
+// let i = 0; //
+
+// {
+//     let counter = 0;
+
+//     while(counter < 10){
+//         document.write("hello" +"<br>");
+//         counter++;
+//     }
+//     while(i<=5){
+
+//         document.write("hello javasript" +"<br>");
+//         i++;
+//     }
+// }
+
+// {
+//     let counter = 0;
+//     let sum = 0;
+    
+//     while(counter <= 80){
+//         if(counter % 2 == 0){
+//             sum = sum + counter;
+//         }
+//         counter++;
+//     }
+//     document.write(sum);
+// }
+
+// Javascript do while loop
+
+// let i = 0;
+// {
+//     let counter = 1;
+//     do{
+//         document.write("hello" +"<br>");
+//         counter++;
+
+//     } 
+//     while(counter < 12);
+//     {
+//         do{
+//             document.write("number:-"+ i + "<br>");
+//             i++;
+//         }
+//         while(i<=6)
+//     }
+
+// }
+
+
+// javascript for loop
+
+// {
+//     for(i=0; i<=10; i++){
+//         document.write("hello" + "<br>")
+//     }
+//     let i = -2;
+//     for(i; i<=0; i++){
+//         document.write("hello" +"<br>")
+//     }
+// }
+
+
+// {
+//     let i = -2;
+
+//     for (i=0; i<=10; i++){  
+//     document.write("hello" + "<br/>")  ;
+//     }  
+//     for(i; i<0; i++){
+//         document.write("hello" + "<br>")
+//     }
+// }
+
+// {
+//     for (let i= 0; i<=5 ; i++) {
+//         document.write('Walking east one step' + "<br>");
+//       }
+// }
+
+
+//                   Loop                    //
+
+// program 03
+
+// let x = 1;
+// let y = 3;
+
+// while(x<=10){
+//     document.write(y + "\tx\t" + x + "\t=" + x*y);
+//     document.write("<br>"); 
+//     x++;
+// }
+
+
+// program 1
+
+
+// let i;
+// i = 10
+// while (i>=0){
+  
+//     console.log(i);
+//     i--;
+// }
+
+// program 2
+
+// {
+//     let x = 1;
+//     let y = 0;
+    
+//     while(x<=10){
+//         y = y + x;
+//         document.write(x);
+//         document.write("<br>");
+//         x++;
+//     }
+//     document.write("total:-\t\t",y);
+
+// }
+
+
+// program 5
+
+// {
+//      let x ;
+//     let y = 0;
+//     let c=1;
+
+//     document.write(y + "<br>");
+//     for(let i=0; i<=5; i++)
+//     {
+//         x=y+c;
+//         y=c;
+//         c=x;
+//         document.write(x + "<br>");   
+//     }
+// }
+
+
+// program 6
 {
-    number1 = 10;
-    number2 = 20;
+    let n=6, i=1, fact=1
 
-if (number1 > number2) {
-    console.log(" bigger = number1");
+    while(i<=n)
+    {
+        fact = fact * i
+        i++
+    }
+    document.write(fact);
+    document.write("<br>")
+
+    for(i=1;i<=n;i++)
+    {
+        fact=fact*i
+    }
+    document.write(fact);
 }
 
-else{
-    console.log("bigger = number2");
-}
-}
